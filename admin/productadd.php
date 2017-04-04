ed61d34c8ec9f23cf216f61f89c283166890bea6
tuhindewan <tuhinsshadow@gmail.com>
2017-04-04 11:04:36 -0700

    add product page

--- admin/productadd.php
+++ admin/productadd.php
@@ -1,5 +1,7 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php include_once '../classes/Category.php'; ?>
+<?php include_once '../classes/Brand.php'; ?>
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Add New Product</h2>
@@ -12,7 +14,7 @@
                         <label>Name</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Enter Product Name..." class="medium" />
+                        <input type="text" name="productName" placeholder="Enter Product Name..." class="medium" />
                     </td>
                 </tr>
 				<tr>
@@ -20,11 +22,22 @@
                         <label>Category</label>
                     </td>
                     <td>
-                        <select id="select" name="select">
+                        <select id="select" name="catId">
+                    
                             <option>Select Category</option>
-                            <option value="1">Category One</option>
-                            <option value="2">Category Two</option>
-                            <option value="3">Category Three</option>
+                            <?php 
+
+                    $cat = new Category();
+                    $getCat = $cat->getAllCat();
+                    if ($getCat) {
+                        while ($result= $getCat->fetch_assoc()) {
+                            
+                        
+                    
+
+                     ?>
+                            <option value="<?php echo $result['catId']; ?>"><?php echo $result['catName']; ?></option>
+                            <?php }} ?>
                         </select>
                     </td>
                 </tr>
@@ -33,11 +46,21 @@
                         <label>Brand</label>
                     </td>
                     <td>
-                        <select id="select" name="select">
+                        <select id="select" name="brandId">
                             <option>Select Brand</option>
-                            <option value="1">Brand One</option>
-                            <option value="2">Brand Two</option>
-                            <option value="3">Brand Three</option>
+                            <?php 
+
+                            $brand = new Brand();
+                            $getBrand = $brand->getAllBrand();
+                            if ($getBrand) {
+                                while ($result=$getBrand->fetch_assoc()) {
+                                    
+                                
+                            
+
+                             ?>
+                            <option value="<?php echo $result['brandId']; ?>"><?php echo $result['brandName']; ?></option>
+                            <?php } } ?>
                         </select>
                     </td>
                 </tr>
@@ -47,7 +70,7 @@
                         <label>Description</label>
                     </td>
                     <td>
-                        <textarea class="tinymce"></textarea>
+                        <textarea class="tinymce" name="body"></textarea>
                     </td>
                 </tr>
 				<tr>
@@ -55,7 +78,7 @@
                         <label>Price</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Enter Price..." class="medium" />
+                        <input type="text" name="price" placeholder="Enter Price..." class="medium" />
                     </td>
                 </tr>
             
@@ -64,7 +87,7 @@
                         <label>Upload Image</label>
                     </td>
                     <td>
-                        <input type="file" />
+                        <input type="file" name="image" />
                     </td>
                 </tr>
 				
@@ -73,10 +96,10 @@
                         <label>Product Type</label>
                     </td>
                     <td>
-                        <select id="select" name="select">
+                        <select id="select" name="type">
                             <option>Select Type</option>
-                            <option value="1">Featured</option>
-                            <option value="2">Non-Featured</option>
+                            <option value="0">Featured</option>
+                            <option value="1">General</option>
                         </select>
                     </td>
                 </tr>
